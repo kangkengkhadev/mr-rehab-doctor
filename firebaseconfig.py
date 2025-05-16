b822cef86d03036b452cbecbd1b0d8d28c94de3b
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2025-05-16 22:48:10 +0700

    make install

--- firebaseconfig.py
+++ firebaseconfig.py
@@ -1,9 +1,13 @@
 import pyrebase
 import firebase_admin 
-from firebase_admin import credentials,firestore
+from firebase_admin import credentials, firestore
+
+# Firebase Admin SDK (for Firestore access)
 cred = credentials.Certificate("rehabtesting-9beaf-firebase-adminsdk-xn4fb-c5da403800.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+# Pyrebase (for Auth and Realtime Database access)
 config = {
   'apiKey': "AIzaSyBPnNnDyGcZmG4Cq-kr3edVwQe5AwthqIE",
   'authDomain': "rehabtesting-9beaf.firebaseapp.com",
@@ -13,5 +17,5 @@ config = {
   'messagingSenderId': "737840427844",
   'appId': "1:737840427844:web:4d934ef2d192ac371ddd11",
 }
-firebase=pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
